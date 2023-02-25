c956777a62f34549373d73e14f1a97e41337343c
Leo Wang <77943019+Le0wang06@users.noreply.github.com>
2023-02-25 20:07:57 +0000

    commit

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    int[][] test1 =
     System.out.println(reverseColumns({}))
   }
   public static void reverseArray(int[] arr){
